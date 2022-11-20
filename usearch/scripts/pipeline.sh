422c959cf36da92cd6555b9aa03dbddbd81e7cb5
NikitaVenediktov <n.venediktov@gmail.com>
2022-11-20 04:07:29 +0300

    mvp without test

--- usearch/scripts/pipeline.sh
+++ usearch/scripts/pipeline.sh
@@ -1,1 +1,12 @@
-# This is a comment
+#!/bin/bash
+# string upper tell to open with bash (not comment)
+
+# Pipline to choose scripts
+
+bash ./megrge \
+    ./filter \
+    ./uniques \
+    ./otus \
+    ./otutable
+
+less -S otutable
