185223058ad8e5d817f69f7713f17cb542a72566
voronik1801 <divi1801@icloud.com>
2022-12-26 23:00:53 +0700

    add taxanomy pipeline

--- pipeline/dada2.R
+++ pipeline/dada2.R
@@ -1,7 +1,6 @@
 # !/usr/bin/env Rscript
 suppressPackageStartupMessages(library("argparse"))
 library(dada2); packageVersion("dada2")
-
 # create parser object
 parser <- ArgumentParser()
 
@@ -20,54 +19,65 @@ parser$add_argument("-p", "--pattern", type = "character", default = "fastq",
 
 args <- parser$parse_args()
 
-fnFs <- sort(list.files(args$forward, 
-                            pattern = args$pattern, 
-                            full.names = TRUE))
+path <- args$forward
 
-sample.names <- sapply(strsplit(basename(fnFs), "\\."), `[`, 1)
-filtFs <- file.path(filtpath, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
-names(filtFs) <- sample.names
+filts <- sort(list.files(path, 
+                        pattern = args$pattern, 
+                        full.names = TRUE))
 
-set.seed(100)
+sample.names <- sapply(strsplit(basename(filts), "\\"), `[`, 1)
+cat(sample.names)
+# filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
+# cat(filtFs)
+# names(filts) <- sample.names
+# cat(names(filts))
 
-errF <- learnErrors(filtFs, multithread=TRUE)
-dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
+# err <- learnErrors(filts, nbases = 1e8, multithread=TRUE, randomize=TRUE)
+
+# dds <- vector("list", length(sample.names))
+# names(dds) <- sample.names
+# for(sam in sample.names) {
+#   cat("Processing:", sam, "\n")
+#   derep <- derepFastq(filts[[sam]])
+#   dds[[sam]] <- dada(derep, err=err, multithread=TRUE)
+# }
+# Construct sequence table and write to disk
+# seqtab <- makeSequenceTable(dds)
+# cat(seqtab)
+# if (args$reverse) {
+#     fnRs <- sort(list.files(args$reverse, 
+#                                 pattern = args$pattern, 
+#                                 full.names = TRUE))
+#     filtRs <- file.path(filtpath, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
+#     names(filtRs) <- sample.names
+
+#     out <- filterAndTrim(fnFs, filtFs, filtRs, fnRs, truncLen=c(240,160),
+#               maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
+#               compress=TRUE, multithread=TRUE)
+#     errR <- learnErrors(filtRs, multithread=TRUE)
+
+#     dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
+#     mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
+#     seqtab <- makeSequenceTable(mergers)
+# } else {
+#     dds <- vector("list", length(sample.names))
+#     names(dds) <- sample.names
+#     for(sam in sample.names) {
+#         cat("Processing:", sam, "\n")
+#         derep <- derepFastq(filtFs[[sam]])
+#         dds[[sam]] <- dada(derep, err=err, multithread=TRUE)
+#     }
+#     seqtab <- makeSequenceTable(dds)
+# }
+
+# saveRDS(seqtab, paste(args$reverse, "otu.rds", sep = "/")) 
 
 
-if (args$reverse) {
-    fnRs <- sort(list.files(args$reverse, 
-                                pattern = args$pattern, 
-                                full.names = TRUE))
-    filtRs <- file.path(filtpath, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
-    names(filtRs) <- sample.names
-
-    out <- filterAndTrim(fnFs, filtFs, filtRs, fnRs, truncLen=c(240,160),
-              maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
-              compress=TRUE, multithread=TRUE)
-    errR <- learnErrors(filtRs, multithread=TRUE)
-
-    dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
-    mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
-    seqtab <- makeSequenceTable(mergers)
-} else {
-    dds <- vector("list", length(sample.names))
-    names(dds) <- sample.names
-    for(sam in sample.names) {
-        cat("Processing:", sam, "\n")
-        derep <- derepFastq(filtFs[[sam]])
-        dds[[sam]] <- dada(derep, err=err, multithread=TRUE)
-    }
-    seqtab <- makeSequenceTable(dds)
-}
-
-saveRDS(seqtab, paste(args$reverse, "otu.rds", sep = "/")) 
-
-
-if (args$type == "taxa"){
-    if (args$database){
-        taxanomy <- assignTaxonomy(seqtab, args$database, multithread=TRUE)
-        saveRDS(taxanomy, paste(args$reverse, "taxa.rds", sep = "/"))
-    } else {
-        cat("Add database for analysis taxanomy.")
-    }
-}
+# if (args$type == "taxa"){
+#     if (args$database){
+#         taxanomy <- assignTaxonomy(seqtab, args$database, multithread=TRUE)
+#         saveRDS(taxanomy, paste(args$reverse, "taxa.rds", sep = "/"))
+#     } else {
+#         cat("Add database for analysis taxanomy.")
+#     }
+# }
