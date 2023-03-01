ee5869135228ba9cbccf2c50c452d2988f09cef3
voronik1801 <divi1801@icloud.com>
2023-03-01 20:59:55 +0600

    refacrote deeploid_cli

--- deeploid_cli.py
+++ deeploid_cli.py
@@ -1,11 +1,10 @@
 import argparse
 import os
 import os.path
-from inspect import getsourcefile
-from datetime import datetime
-import string
 import random
-
+import string
+from datetime import datetime
+from inspect import getsourcefile
 
 
 def config_maker(settings, config_file):
@@ -34,12 +33,11 @@ def main(settings):
     '''
     Runs snakemeake
     '''
-    #Snakemake
     command = f"""
     snakemake --snakefile {settings["working_dir"]}/workflow/snakefile \
               --configfile {settings["config_file"]} \
               --cores {settings["threads"]} \
-              --use-conda qiime2"""
+              --use-conda {settings["tools"]}"""
     print(command)
     os.system(command)
     print('snakemake runs...')
@@ -47,7 +45,8 @@ def main(settings):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='deeploid pipeline for 16s rrna data analysis')
+    parser = argparse.ArgumentParser(
+        description='deeploid pipeline for 16s rrna data analysis')
 
     parser.add_argument(
         '-t', '--tool',
@@ -74,29 +73,26 @@ if __name__ == '__main__':
         '-nt', '--threads', help='Number of threads (default = 8)',
         required=False, default=int(8), type=int)
 
-
-
     args = vars(parser.parse_args())
 
     execution_folder = args["working_dir"]
-    execution_folder_danila = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+    execution_folder_danila = os.path.dirname(
+        os.path.abspath(getsourcefile(lambda: 0)))
     execution_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    random_letters = "".join([random.choice(string.ascii_letters) for n in range(3)])
-    config_file = os.path.join(execution_folder, f"config/config_{random_letters}{execution_time}.yaml")
-
-
+    random_letters = "".join(
+        [random.choice(string.ascii_letters) for n in range(3)])
+    # пока что не вижу смысла создавать по времени конфиг, это все решится на стороне бэка
+    config_file = os.path.join(execution_folder, f"config/config.yaml")
     settings = {
         'tool': args["tool"],
         'forward_reads': args["forward_reads"],
-        'reverse_reads' : args["reverse_reads"],
-        'outdir' : args["outdir"],
-        'database' : args["database"],
-        'threads' : args["threads"],
+        'reverse_reads': args["reverse_reads"],
+        'outdir': args["outdir"],
+        'database': args["database"],
+        'threads': args["threads"],
         'working_dir': execution_folder,
-        'config_file' : config_file
+        'config_file': config_file
     }
 
-    # print(settings)
-    # print(type(settings["threads"]))
     config_maker(settings, config_file)
     main(settings)
